fc81e595ced26518a19f8c4a168a2721f6d01c9f
Marcel Bischoff <mrclbschff@gmail.com>
2023-12-13 00:49:57 -0500

    works

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ocontexto
+
+## Run
+```sh
+dune exec app --watch
+```
+
+## Deploy
+```sh
+docker-compose build && \
+fly deploy --local-only
+```
+
+
--- bin/main.ml
+++ bin/main.ml
@@ -1,12 +1,11 @@
 open Jingoo
 
-type keyword_record = { 
-    keyword : string; 
-    _similarity : float; 
-    _index : int;
-    _rank : int
+type keyword_record = {
+  keyword : string;
+  similarity : float;
+  index : int;
+  rank : int;
 }
-
 
 let max_search_terms = 10
 let ( >>= ) = Option.bind
@@ -24,9 +23,9 @@ let load_keyword_records file =
   let map_to_kw a b =
     {
       keyword = fst b;
-      _similarity = fst @@ snd b;
-      _index = a;
-      _rank = snd @@ snd b;
+      similarity = fst @@ snd b;
+      index = a;
+      rank = snd @@ snd b;
     }
   in
   List.combine keywords (List.combine similarities ranks)
@@ -37,7 +36,7 @@ let keyword_records_to_keyword_map keyword_records =
   keyword_records |> Array.to_seq |> Seq.map imap |> StringMap.of_seq
 
 let keyword_records = load_keyword_records "data.json"
-let _keyword_map = keyword_records_to_keyword_map keyword_records
+let keyword_map = keyword_records_to_keyword_map keyword_records
 
 let search search_term =
   let sw = String.lowercase_ascii search_term in
@@ -55,6 +54,18 @@ let char_to_id_map =
   String.to_seq code |> Seq.mapi (fun a b -> (b, a)) |> CharMap.of_seq
 
 let char_to_idx (c : char) = CharMap.find_opt c char_to_id_map
+let idx_to_char (i : int) = String.get code i
+
+let encode (i : int) : string option =
+  match 0 <= i && i < length * length with
+  | false -> None
+  | true ->
+      let s1 = idx_to_char (i / length) |> Char.escaped in
+      let s2 = idx_to_char (i mod length) |> Char.escaped in
+      Some (s1 ^ s2)
+
+let encode_all (state_rep : int list) : string =
+  state_rep |> List.filter_map encode |> String.concat ""
 
 let decode (s : string) =
   let a = char_to_idx (String.get s 0) in
@@ -76,29 +87,58 @@ let decode_all s =
   in
   List.rev (aux [] s)
 
-let post state request = (
-             match%lwt Dream.form ~csrf:false request with
-             | `Ok [ ("search", s) ] ->
+let keyword_to_idx kw =
+  let get_index k = Some k.index in
+  keyword_map |> StringMap.find_opt kw >>= get_index
+
+let get_current_keywords lst =
+  let imap i = Array.get keyword_records i in
+  let cmp kw1 kw2 = compare kw2.similarity kw1.similarity in
+  lst |> List.map imap |> List.sort cmp
+
+let post state request =
+  match%lwt Dream.form ~csrf:false request with
+  | `Ok [ ("search", s) ] -> (
+      (*
                  Jg_template.from_string "{{ s }}"
                    ~models:[ ("s", Jg_types.Tstr ( s ^ "state=" ^ state)) ]
-                 |> Dream.html
-             | _ -> Dream.empty `Bad_Request
-)
+                 |> Dream.html *)
+      let () = print_endline s in
+      let idx = keyword_to_idx s in
+      let decoded = decode_all state in
+      let state_rep = List.map Result.to_list decoded |> List.flatten in
+      match idx with
+      | Some i ->
+          let new_state = i :: state_rep |> encode_all in
+          Dream.redirect request ("/" ^ new_state)
+      | None ->
+          Jg_template.from_file "templates/main.html"
+            ~models:
+              [
+                ("state", Jg_types.Tstr state);
+                ("search", Jg_types.Tstr "Word not found!!");
+              ]
+          |> Dream.html)
+  | _ -> Dream.empty `Bad_Request
 
-let search state request = (
-             match%lwt Dream.form ~csrf:false request with
-             | `Ok [ ("search", search_term) ] ->
-                 let active_item search =
-                   Jg_template.from_file "templates/active.html"
-                     ~models:[ 
-                         ("search", Jg_types.Tstr search);
-                         ("state", Jg_types.Tstr state)
-                    ]
-                 in
-                 search search_term |> List.map active_item |> String.concat ""
-                 |> Dream.html
-             | _ -> Dream.empty `Bad_Request
-)
+let search state request =
+  match%lwt Dream.form ~csrf:false request with
+  | `Ok [ ("search", search_term) ] ->
+      let active_item search =
+        Jg_template.from_file "templates/active.html"
+          ~models:
+            [ ("search", Jg_types.Tstr search); ("state", Jg_types.Tstr state) ]
+      in
+      search search_term |> List.map active_item |> String.concat ""
+      |> Dream.html
+  | _ -> Dream.empty `Bad_Request
+
+let format keyword_record =
+  keyword_record.keyword ^ ": "
+  ^ string_of_int keyword_record.rank
+  ^ " ("
+  ^ Float.to_string keyword_record.similarity
+  ^ ")"
 
 let () =
   Dream.run ~interface:"0.0.0.0"
@@ -109,15 +149,27 @@ let () =
              Jg_template.from_file "templates/main.html" |> Dream.html);
          Dream.get "/:state" (fun request ->
              let state = Dream.param request "state" in
-             Jg_template.from_file "templates/main.html" 
-             ~models:[("state", Jg_types.Tstr state)]|> Dream.html);
-         Dream.post "/search/" (fun request ->
-             search "" request);
+             Jg_template.from_file "templates/main.html"
+               ~models:[ ("state", Jg_types.Tstr state) ]
+             |> Dream.html);
+         Dream.get "/keywords/:state" (fun request ->
+             let state = Dream.param request "state" in
+             let decoded = decode_all state in
+             let state_rep = List.map Result.to_list decoded |> List.flatten in
+             let current = get_current_keywords state_rep in
+             match current with
+             | best :: _ when best.similarity = 1.0 -> 
+                Jg_template.from_file "templates/win.html" |> Dream.html
+             | _ ->
+                 let current_keywords =
+                   current |> List.map format |> String.concat "<br>"
+                 in
+                 Dream.html current_keywords);
+         Dream.post "/search/" (fun request -> search "" request);
          Dream.post "/search/:state" (fun request ->
              let state = Dream.param request "state" in
              search state request);
-         Dream.post "/" (fun request ->
-             post "" request);
+         Dream.post "/" (fun request -> post "" request);
          Dream.post "/:state" (fun request ->
              let state = Dream.param request "state" in
              post state request);
